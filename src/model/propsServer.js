e4e96ba0b9e628e0677809c31da5f1a49a081d6b
GustavoAngel <gustavoandradeangel@hotmail.com>
2022-12-02 11:17:04 -0600

    url update

--- src/model/propsServer.js
+++ src/model/propsServer.js
@@ -1,4 +1,5 @@
 export default class propsServer {
     //Prop with route de api public    
-    static urlApi= 'http://localhost:3000/';
+    static urlApi= 'https://pruebatecnicabackend.herokuapp.com/';
+    //  static urlApi= 'http://localhost:3000/';
 }
